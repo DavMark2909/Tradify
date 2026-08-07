c2c6a914080e1ce02059cb89dcc2701df34ccc5c
DavMark2909 <77192509+DavMark2909@users.noreply.github.com>
2026-08-07 00:12:52 -0700

    this commit for jwt tokens, the previous one update the SecurityUser

--- authorization/src/main/java/com/tradify/authorization/config/SecurityConfig.java
+++ authorization/src/main/java/com/tradify/authorization/config/SecurityConfig.java
@@ -5,17 +5,22 @@ import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
+import com.tradify.authorization.security.SecurityUser;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.OAuth2TokenType;
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
 
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
@@ -56,6 +61,24 @@ public class SecurityConfig {
 
         JWKSet jwkSet = new JWKSet(rsaKey);
         return new ImmutableJWKSet<>(jwkSet);
+    }
+
+    @Bean
+    public OAuth2TokenCustomizer<JwtEncodingContext> jwtTokenCustomizer() {
+        return context -> {
+            if (OAuth2TokenType.ACCESS_TOKEN.equals(context.getTokenType())) {
+                Authentication principal = context.getPrincipal();
+
+                // need to get my implementaion of UserDetails
+                if (principal.getPrincipal() instanceof SecurityUser securityUser) {
+
+                    // creating custom claims to be extracted in resource server
+                    context.getClaims().claim("username", securityUser.getUsername());
+                    context.getClaims().claim("name", securityUser.getFirstName());
+                    context.getClaims().claim("lastname", securityUser.getLastName());
+                }
+            }
+        };
     }
 
     private static KeyPair generateRsaKey() {
