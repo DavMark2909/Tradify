f3dfb622d8c4992a6291e778e2e1e79b2b6bff48
DavMark2909 <77192509+DavMark2909@users.noreply.github.com>
2026-08-06 23:48:40 -0700

    created not found exception

---
+++ application/src/main/java/com/tradify/application/exception/ObjectNotFoundException.java
@@ -0,0 +1,7 @@
+package com.tradify.application.exception;
+
+public class ObjectNotFoundException extends Exception {
+    public ObjectNotFoundException(String message) {
+        super(message);
+    }
+}
