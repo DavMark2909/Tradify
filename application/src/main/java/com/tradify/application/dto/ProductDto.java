4691ca6474d1a9b1270db1a5a4ca939e865768f3
DavMark2909 <77192509+DavMark2909@users.noreply.github.com>
2026-08-14 23:26:00 -0700

    added dto-s for products and saved items

--- application/src/main/java/com/tradify/application/dto/ProductDto.java
+++ application/src/main/java/com/tradify/application/dto/ProductDto.java
@@ -1,4 +1,17 @@
 package com.tradify.application.dto;
 
-public class ProductDto {
-}
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public record ProductDto (
+        long id,
+        String title,
+        String description,
+        BigDecimal price,
+        String currency,
+        String measure,
+        BigDecimal quantity,
+        String status,
+        String company,
+        LocalDateTime createdAt
+) {}
--- application/src/main/java/com/tradify/application/dto/SavedItemDto.java
+++ application/src/main/java/com/tradify/application/dto/SavedItemDto.java
@@ -1,4 +1,18 @@
 package com.tradify.application.dto;
 
-public record SavedItemDto() {
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public record SavedItemDto(
+        long id,
+        String title,
+        String description,
+        BigDecimal price,
+        String currency,
+        String measure,
+        BigDecimal quantity,
+        String status,
+        String company,
+        LocalDateTime addedAt
+) {
 }
