20cc2ccc429584a9acb64b09b6d510c4c5764163
DavMark2909 <77192509+DavMark2909@users.noreply.github.com>
2026-09-13 23:34:48 -0700

    added default methods to sector service

--- application/src/main/java/com/tradify/application/service/SectorService.java
+++ application/src/main/java/com/tradify/application/service/SectorService.java
@@ -7,9 +7,13 @@ import com.tradify.application.repository.SectorRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class SectorService {
+
+    public static final String DEFAULT_BUYER_SECTOR_NAME = "Buyer";
 
     private final SectorRepository sectorRepository;
 
@@ -17,11 +21,21 @@ public class SectorService {
         return sectorRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Sector not found for id " + id));
     }
 
-//    TODO: it is important to add some default sectors
+    public Sector findDefaultBuyerSector() throws ObjectNotFoundException {
+        return sectorRepository.findByName(DEFAULT_BUYER_SECTOR_NAME)
+                .orElseThrow(() -> new ObjectNotFoundException("Default buyer sector not found"));
+    }
+
     public Sector save(SectorDto sectorDto) {
         Sector sector = new Sector();
         sector.setName(sectorDto.name());
         sector.setDescription(sectorDto.description());
         return sectorRepository.save(sector);
+    }
+
+    public List<SectorDto> getAllSectors(){
+        return sectorRepository.findAll().stream()
+                .map(s -> new SectorDto(s.getId(), s.getName(), s.getDescription()))
+                .toList();
     }
 }
